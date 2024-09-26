5d0998e7822b23cf9a9a89fb53e3e23938dff096
tanekirl <tanekirl@COMPUTER>
2024-09-26 16:00:13 +0300

    Добавьте файлы проекта.

---
+++ AS1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AS1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
